fdbdc9eb796e96f21095517b1677b0746ac4b352
Praveesha De Silva <praveeshadesilva@gmail.com>
2025-04-26 01:48:20 +0530

    fix: fixed the ThemeContext import error

--- src/components/themeToggle/themeToggle.jsx
+++ src/components/themeToggle/themeToggle.jsx
@@ -1,7 +1,9 @@
+"use client"
+
 import styles from './themeToggle.module.css';
 import Image from 'next/image';
 import { useContext } from 'react';
-import { ThemeContext } from '../../context/themeContext';
+import { ThemeContext } from '@/context/themeContext';
 
 const ThemeToggle = () => {
   const { toggle, theme } = useContext(ThemeContext);
@@ -14,8 +16,8 @@ const ThemeToggle = () => {
 
       <Image src="/moon.png" alt="Moon Icon" width={14 } height={14} className={styles.icon} />
 
-      //toggle container's color adjusting with respect to the theme
-      <div className={styles .ball} 
+      {/*toggle container's color adjusting with respect to the theme*/}      
+      <div className={styles.ball} 
       style={
         theme === "dark" ? 
         { left: 1, background: "#0f172a" }
